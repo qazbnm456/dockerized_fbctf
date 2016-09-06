53546d7bdf4b0162ff14653ef97ff5c9fb6e787c
Lobsiinvok <qazbnm456@gmail.com>
2016-09-06 20:25:03 +0800

    close

--- log_forwarding.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Forwards the Nginx access.log to the Docker logs.
-set -e
-if [[ -e /var/log/nginx/access.log ]]; then
-        exec tail -F /var/log/nginx/access.log
-else
-        exec sleep 10
-fi
